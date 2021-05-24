42ffcaeeb8385a03b82e412b81ec1f74b1787a41
Farley Reis <farleyhard@hotmail.com>
2021-05-24 12:28:15 +0100

    Porject initial

---
+++ src/test/java/demomple/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package demomple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
